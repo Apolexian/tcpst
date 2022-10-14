5fa56d0f1ff57b29b76e858efe7955dda14c8b52
Ivan Nikitin <ivan@niktivan.org>
2022-10-14 13:20:58 +0100

    WIP: add sender and dispatch for send

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![warn(clippy::all, clippy::pedantic, clippy::nursery)]
 
 use std::marker::PhantomData;
+
 /// TODO:
 /// 1) Need some sort of way to represent a session type, i.e. encode the TCP state machine in types
 ///
@@ -16,18 +17,58 @@ pub trait Message: PartialEq + Ord + Default {}
 pub trait Action {
     type Dual;
     type Cont;
+
+    fn get_cont(&self) -> Self::Cont;
+    fn new() -> Self;
 }
+
+pub struct Sender<M: Message, A: Action> {
+    phantom: PhantomData<A>,
+    dispatch: Box<dyn Fn(M)>,
+}
+
+impl<M, A> Sender<M, A>
+where
+    M: Message,
+    A: Action + Action<Cont = A>,
+{
+    pub fn new(dispatch: Box<dyn Fn(M)>) -> Self {
+        Sender {
+            phantom: PhantomData::default(),
+            dispatch,
+        }
+    }
+
+    pub fn send(&self, message: M, action: A) -> A
+    where
+        <A as Action>::Cont: Action,
+    {
+        (self.dispatch)(message);
+        action.get_cont()
+    }
+}
+
 pub struct Send<M: Message, A: Action> {
     phantom: PhantomData<(M, A)>,
 }
 
 impl<M: Message, A> Action for Send<M, A>
 where
-    A: Action,
+    A: Action + Action<Cont = A>,
     <A as Action>::Dual: Action,
 {
     type Dual = Recv<M, A::Dual>;
     type Cont = A;
+
+    fn new() -> Self {
+        Send {
+            phantom: PhantomData::default(),
+        }
+    }
+
+    fn get_cont(&self) -> Self::Cont {
+        <A::Cont as Action>::new()
+    }
 }
 
 pub struct Offer<A: Action, O: Action> {
@@ -43,6 +84,14 @@ where
 {
     type Dual = Choose<A::Dual, O::Dual>;
     type Cont = A;
+
+    fn get_cont(&self) -> Self::Cont {
+        todo!()
+    }
+
+    fn new() -> Self {
+        todo!()
+    }
 }
 
 pub struct Choose<A: Action, O: Action> {
@@ -58,6 +107,14 @@ where
 {
     type Dual = Offer<A::Dual, O::Dual>;
     type Cont = A;
+
+    fn get_cont(&self) -> Self::Cont {
+        todo!()
+    }
+
+    fn new() -> Self {
+        todo!()
+    }
 }
 
 pub struct Recv<M: Message, A: Action> {
@@ -69,6 +126,14 @@ pub struct Terminate {}
 impl Action for Terminate {
     type Dual = Terminate;
     type Cont = Terminate;
+
+    fn get_cont(&self) -> Self::Cont {
+        todo!()
+    }
+
+    fn new() -> Self {
+        todo!()
+    }
 }
 
 impl<M: Message, A> Action for Recv<M, A>
@@ -78,6 +143,14 @@ where
 {
     type Dual = Send<M, A::Dual>;
     type Cont = A;
+
+    fn get_cont(&self) -> Self::Cont {
+        todo!()
+    }
+
+    fn new() -> Self {
+        todo!()
+    }
 }
 
 pub enum Branch<L: Action, R: Action> {
