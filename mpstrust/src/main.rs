657b4492574d22ee50f16a9baf358ca0a452fc5e
Ivan Nikitin <ivan@niktivan.org>
2023-03-20 22:53:00 +0000

    RST issue
    I think the Kernel issues an RST when it should not causing the
    handshake to not complete.
    
    Trying to use iptables to make kernel ignore the port, but this does not
    seem to work on wsl.
    
    tcpdump: listening on lo, link-type EN10MB (Ethernet), snapshot length 262144 bytes
    22:39:36.057615 IP (tos 0x0, ttl 64, id 4759, offset 0, flags [DF], proto TCP (6), length 4115)
        192.168.0.1.49155 > 192.168.0.1.541: Flags [S], cksum 0x1cd4 (correct), seq 1:4064, win 4015, options [eol], length 4063
    22:39:36.057623 IP (tos 0x0, ttl 64, id 0, offset 0, flags [DF], proto TCP (6), length 44)
        192.168.0.1.541 > 192.168.0.1.49155: Flags [S.], cksum 0x8171 (incorrect -> 0xc0e8), seq 3062948651, ack 2, win 65495, options [mss 65495], length 0
    22:39:36.057626 IP (tos 0x0, ttl 64, id 0, offset 0, flags [DF], proto TCP (6), length 40)
        192.168.0.1.49155 > 192.168.0.1.541: Flags [R], cksum 0x6c6b (correct), seq 2, win 0, length 0
    22:39:36.057633 IP (tos 0x0, ttl 64, id 4760, offset 0, flags [DF], proto TCP (6), length 4096)
        192.168.0.1.541 > 192.168.0.1.49155: Flags [.], cksum 0x1cd9 (correct), seq 1232018646:1232022690, ack 4294967295, win 4015, options [eol], length 4044
    22:39:36.057635 IP (tos 0x0, ttl 64, id 0, offset 0, flags [DF], proto TCP (6), length 40)
        192.168.0.1.49155 > 192.168.0.1.541: Flags [R], cksum 0x6c6d (correct), seq 0, win 0, length 0

--- mpstrust/src/main.rs
+++ mpstrust/src/main.rs
@@ -1,11 +1,15 @@
-use mpstrust::{Role};
-use pnet::packet::ip::IpNextHeaderProtocols;
+use std::net::{IpAddr, Ipv4Addr};
 
-use pnet::packet::{Packet};
+use mpstrust::Role;
+use pnet::packet::ip::IpNextHeaderProtocols::{self, Tcp};
+use pnet::packet::{MutablePacket, Packet};
+
+use pnet::packet::tcp::{ipv4_checksum, MutableTcpPacket, TcpFlags, TcpPacket};
 use pnet::transport::tcp_packet_iter;
+use pnet::transport::transport_channel;
 use pnet::transport::TransportChannelType::Layer4;
 use pnet::transport::TransportProtocol::Ipv4;
-use pnet::transport::{transport_channel};
+extern crate pnet;
 
 pub struct RoleServerSystem {}
 impl Role for RoleServerSystem {}
@@ -17,8 +21,9 @@ pub struct RoleServerClient {}
 impl Role for RoleServerClient {}
 
 fn main() {
+    let addr = IpAddr::V4(Ipv4Addr::new(192, 168, 0, 1));
     let protocol = Layer4(Ipv4(IpNextHeaderProtocols::Tcp));
-    let (_, mut rx) = match transport_channel(4096, protocol) {
+    let (mut tx, mut rx) = match transport_channel(4096, protocol) {
         Ok((tx, rx)) => (tx, rx),
         Err(e) => panic!(
             "An error occurred when creating the transport channel: {}",
@@ -27,20 +32,57 @@ fn main() {
     };
 
     let mut iter = tcp_packet_iter(&mut rx);
-    loop {
-        match iter.next() {
-            Ok((packet, _addr)) => {
-                let parsed = smoltcp::wire::TcpPacket::new_checked(packet.packet()).unwrap();
-                // filter out other broadcast traffic
-                if parsed.dst_port() != 541 {
-                    continue;
-                }
-                eprintln!("{:?}", parsed.syn());
+    let mut buffer = [0; 4095];
+    let mut syn_packet = MutableTcpPacket::new(&mut buffer).unwrap();
+    syn_packet.set_flags(TcpFlags::SYN);
+    syn_packet.set_sequence(1);
+    //  iptables -t raw -A PREROUTING -p tcp --dport 41955 -j DROP 
+    syn_packet.set_source(49155);
+    syn_packet.set_destination(541);
+    syn_packet.set_window(4015);
+    syn_packet.set_data_offset(8);
+    let checksum = ipv4_checksum(
+        &syn_packet.to_immutable(),
+        &Ipv4Addr::new(192, 168, 0, 1),
+        &Ipv4Addr::new(192, 168, 0, 1),
+    );
+    syn_packet.set_checksum(checksum);
+    tx.send_to(syn_packet, addr).unwrap();
+
+    match iter.next() {
+        Ok((packet, _)) => {
+            // if packet is not for us then something went wrong
+            if !packet.get_destination() == 49155 {
+                panic!("Deal with packet from wrong source...")
             }
-            Err(e) => {
-                // If an error occurs, we can handle it here
-                panic!("An error occurred while reading: {}", e);
+            if !packet.get_acknowledgement() == 2 {
+                panic!("Deal with wrong ack numbers here...")
             }
+
+            let mut vec: Vec<u8> = vec![0; packet.packet().len()];
+            let mut new_packet = MutableTcpPacket::new(&mut vec[..]).unwrap();
+            new_packet.set_flags(TcpFlags::ACK);
+            new_packet.set_sequence(packet.get_acknowledgement() + 1);
+            new_packet.set_source(packet.get_destination());
+            new_packet.set_destination(packet.get_source());
+            new_packet.set_window(4015);
+            new_packet.set_data_offset(8);
+            let checksum = ipv4_checksum(
+                &new_packet.to_immutable(),
+                &Ipv4Addr::new(192, 168, 0, 1),
+                &Ipv4Addr::new(192, 168, 0, 1),
+            );
+            new_packet.set_checksum(checksum);
+
+            // Send the packet
+            match tx.send_to(new_packet, addr) {
+                Ok(n) => assert_eq!(n, packet.packet().len()),
+                Err(e) => panic!("failed to send packet: {}", e),
+            }
+        }
+        Err(e) => {
+            // If an error occurs, we can handle it here
+            panic!("An error occurred while reading: {}", e);
         }
     }
 }
