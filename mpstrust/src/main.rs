53a676af267cec353362207f95df1de460dfaae8
Ivan Nikitin <ivan@niktivan.org>
2023-03-21 01:55:45 +0000

    Got the SYN-ACK to go through and ACK comes back
    The server impl seems to be correctly constructing a synack now and
    sending it.
    We also get an ack back so it seems that the kernel does not intercept
    the packet on the way anymore.
    
    tcpdump: listening on lo, link-type EN10MB (Ethernet), snapshot length 262144 bytes
    
    01:11:42.468491 IP (tos 0x0, ttl 64, id 44587, offset 0, flags [DF], proto TCP (6), length 60)
    localhost.50216 > localhost.49155: Flags [S], cksum 0xfe30 (incorrect -> 0x35c3), seq 1061893115, win 65495, options [mss 65495,sackOK,TS val 2295437818 ecr 0,nop,wscale 7], length 0
    
    01:11:42.468753 IP (tos 0x0, ttl 64, id 31266, offset 0, flags [DF], proto TCP (6), length 60)
    localhost.49155 > localhost.50216: Flags [S.], cksum 0x7a98 (correct), seq 1:9, ack 1061893116, win 4015, options [eol], length 8
    
    01:11:42.468782 IP (tos 0x0, ttl 64, id 44588, offset 0, flags [DF], proto TCP (6), length 40)
    localhost.50216 > localhost.49155: Flags [.], cksum 0xfe1c (incorrect -> 0xba84), ack 1, win 65495, length 0

--- mpstrust/src/main.rs
+++ mpstrust/src/main.rs
@@ -1,4 +1,5 @@
 use std::net::{IpAddr, Ipv4Addr};
+use std::ops::Add;
 
 use mpstrust::Role;
 use pnet::packet::ip::IpNextHeaderProtocols::{self, Tcp};
@@ -9,6 +10,7 @@ use pnet::transport::tcp_packet_iter;
 use pnet::transport::transport_channel;
 use pnet::transport::TransportChannelType::Layer4;
 use pnet::transport::TransportProtocol::Ipv4;
+use raw_socket::{Domain, Protocol, Type};
 extern crate pnet;
 
 pub struct RoleServerSystem {}
@@ -21,7 +23,22 @@ pub struct RoleServerClient {}
 impl Role for RoleServerClient {}
 
 fn main() {
-    let addr = IpAddr::V4(Ipv4Addr::new(192, 168, 0, 1));
+    let remote_addr = Ipv4Addr::new(127, 0, 0, 1);
+    let local_addr = Ipv4Addr::new(127, 0, 0, 1);
+    let remote_addr_v4 = IpAddr::V4(remote_addr);
+
+    let _destination_port = 40000_u16;
+    let source_port = 49155_u16;
+
+    // Silly trick to make the kernel not process TCP packets
+    // this is used in combination with `iptables -A OUTPUT -p tcp --tcp-flags RST RST -j DROP`,
+    // which drops any outgoing RST segments that the kernel tries to send.
+    // This socket is never used again after but it means that the kernel will not try to process incoming segments.
+    // https://stackoverflow.com/questions/31762305/prevent-kernel-from-processing-tcp-segments-bound-to-a-raw-socket
+    let socket =
+        raw_socket::RawSocket::new(Domain::ipv4(), Type::stream(), Some(Protocol::tcp())).unwrap();
+    socket.bind(("127.0.0.1", source_port)).unwrap();
+
     let protocol = Layer4(Ipv4(IpNextHeaderProtocols::Tcp));
     let (mut tx, mut rx) = match transport_channel(4096, protocol) {
         Ok((tx, rx)) => (tx, rx),
@@ -32,57 +49,46 @@ fn main() {
     };
 
     let mut iter = tcp_packet_iter(&mut rx);
-    let mut buffer = [0; 4095];
-    let mut syn_packet = MutableTcpPacket::new(&mut buffer).unwrap();
-    syn_packet.set_flags(TcpFlags::SYN);
-    syn_packet.set_sequence(1);
-    //  iptables -t raw -A PREROUTING -p tcp --dport 41955 -j DROP 
-    syn_packet.set_source(49155);
-    syn_packet.set_destination(541);
-    syn_packet.set_window(4015);
-    syn_packet.set_data_offset(8);
-    let checksum = ipv4_checksum(
-        &syn_packet.to_immutable(),
-        &Ipv4Addr::new(192, 168, 0, 1),
-        &Ipv4Addr::new(192, 168, 0, 1),
-    );
-    syn_packet.set_checksum(checksum);
-    tx.send_to(syn_packet, addr).unwrap();
+    let got_syn = false;
+    loop {
+        match iter.next() {
+            Ok((packet, _)) => {
+                // if packet is not for us then something went wrong
+                if !(packet.get_destination() == 49155) {
+                    continue;
+                }
+                if !got_syn {
+                    eprintln!("got packet...");
+                    eprintln!("{:?}", packet.get_destination());
+                    eprintln!("{:?}", packet.get_sequence());
+                    eprintln!("{:?}", packet.get_source());
 
-    match iter.next() {
-        Ok((packet, _)) => {
-            // if packet is not for us then something went wrong
-            if !packet.get_destination() == 49155 {
-                panic!("Deal with packet from wrong source...")
-            }
-            if !packet.get_acknowledgement() == 2 {
-                panic!("Deal with wrong ack numbers here...")
-            }
+                    let mut vec: Vec<u8> = vec![0; packet.packet().len()];
+                    let mut new_packet = MutableTcpPacket::new(&mut vec[..]).unwrap();
+                    new_packet.set_flags(TcpFlags::ACK | TcpFlags::SYN);
+                    new_packet.set_sequence(1);
+                    new_packet.set_acknowledgement(packet.get_sequence().add(1));
+                    new_packet.set_source(packet.get_destination());
+                    new_packet.set_destination(packet.get_source());
+                    new_packet.set_window(4015);
+                    new_packet.set_data_offset(8);
+                    let checksum =
+                        ipv4_checksum(&new_packet.to_immutable(), &local_addr, &remote_addr);
+                    new_packet.set_checksum(checksum);
 
-            let mut vec: Vec<u8> = vec![0; packet.packet().len()];
-            let mut new_packet = MutableTcpPacket::new(&mut vec[..]).unwrap();
-            new_packet.set_flags(TcpFlags::ACK);
-            new_packet.set_sequence(packet.get_acknowledgement() + 1);
-            new_packet.set_source(packet.get_destination());
-            new_packet.set_destination(packet.get_source());
-            new_packet.set_window(4015);
-            new_packet.set_data_offset(8);
-            let checksum = ipv4_checksum(
-                &new_packet.to_immutable(),
-                &Ipv4Addr::new(192, 168, 0, 1),
-                &Ipv4Addr::new(192, 168, 0, 1),
-            );
-            new_packet.set_checksum(checksum);
-
-            // Send the packet
-            match tx.send_to(new_packet, addr) {
-                Ok(n) => assert_eq!(n, packet.packet().len()),
-                Err(e) => panic!("failed to send packet: {}", e),
+                    // Send the packet
+                    match tx.send_to(new_packet, remote_addr_v4) {
+                        Ok(n) => assert_eq!(n, packet.packet().len()),
+                        Err(e) => panic!("failed to send packet: {}", e),
+                    }
+                } else {
+                    eprintln!("todo!");
+                }
             }
-        }
-        Err(e) => {
-            // If an error occurs, we can handle it here
-            panic!("An error occurred while reading: {}", e);
+            Err(e) => {
+                // If an error occurs, we can handle it here
+                panic!("An error occurred while reading: {}", e);
+            }
         }
     }
 }
